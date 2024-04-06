4d2a0faf630bc9741b43cd2201efb52fe1cd331a
JessePinkman <bum8hj123@gmail.com>
2024-04-05 22:44:27 -0400

    Implement 'delete post' functionality

--- src/posts/static/posts/detail.js
+++ src/posts/static/posts/detail.js
@@ -1,4 +1,5 @@
 console.log("hello world detail")
+console.log(window.location.href)
 
 const postBox = document.getElementById('post-box')
 const alertBox = document.getElementById('alert-box')
@@ -91,4 +92,19 @@ updateForm.addEventListener('submit', (e) => {
 
 deleteForm.addEventListener('submit', (e) => {
 	e.preventDefault()
+
+	$.ajax({
+		type: 'POST',
+		url: deleteUrl,
+		data: {
+			'csrfmiddlewaretoken': csrf[0].value
+		},
+		success: function(response) {
+			window.location.href = window.location.origin
+			localStorage.setItem('title', titleInput.value)
+		},
+		error: function(error) {
+			console.log(error)
+		}
+	})
 })
