20e96a8bd50804433d18e5693e667aa410708a5e
JessePinkman <bum8hj123@gmail.com>
2024-04-05 22:29:54 -0400

    Write update and delete views

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -3,6 +3,8 @@ from .views import (
 	post_list_and_create,
 	like_unlike_post,
 	post_detail,
+	update_post,
+	delete_post,
 
 	load_posts_data_view,
 	post_detail_data_view,
@@ -14,6 +16,8 @@ urlpatterns = [
 	path('', post_list_and_create, name='main-board'),
 	path('like-unlike/', like_unlike_post, name='like-unlike'),
 	path('<pk>/', post_detail, name='post-detail'),
+	path('<pk>/update/', update_post, name='post-update'),
+	path('<pk>/delete/', delete_post, name='post-delete'),
 
 	path('data/<int:num_posts>/', load_posts_data_view, name='posts-data'),
 	path('<pk>/data/', post_detail_data_view, name='post-detail-data'),
--- src/posts/views.py
+++ src/posts/views.py
@@ -92,5 +92,26 @@ def like_unlike_post(request):
 
 		return JsonResponse({'liked': liked, 'like_count': obj.like_count})
 
-def hello_world_view(request):
-	return JsonResponse({'text': 'hello world x2'})
+def update_post(request, pk):
+	obj = Post.objects.get(pk=pk)
+
+	if request.headers.get('x-requested-with') == 'XMLHttpRequest': # equivalent to request.is_ajax()
+		new_title = request.POST.get('title')
+		new_body = request.POST.get('body')
+
+		obj.title = new_title
+		obj.body = new_body
+		obj.save()
+
+	return JsonResponse({
+		'title': obj.title,
+		'body': obj.body
+	})
+
+def delete_post(request, pk):
+	obj = Post.objects.get(pk=pk)
+
+	if request.headers.get('x-requested-with') == 'XMLHttpRequest': # equivalent to request.is_ajax()
+		obj.delete()
+
+	return JsonResponse({})
